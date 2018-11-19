8ca19395fe765f05656e85af0129cd0ee4eca795
Breezy <pbrowning11@gmail.com>
2018-11-19 16:58:59 -0500

    more changes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,8 @@
-import express from "express";
-// import userRoutes from "./users";
+import express from "express"
+// import authRoutes from "./auth"
 
 const router = express.Router();
 
-// router.use("/users", userRoutes);
+// router.use("/auth", authRoutes);
 
-export default router;
+export default  router;
